c4787dbe00c845cefb2c705b1119200e35cd2ec9
Anwesh43 <anweshthecool0@gmail.com>
2020-08-20 04:13:15 +0530

    adding StepReflectedBallView class

--- stepreflectedballview/src/main/java/com/anwesh/uiprojects/stepreflectedballview/StepReflectedBallView.kt
+++ stepreflectedballview/src/main/java/com/anwesh/uiprojects/stepreflectedballview/StepReflectedBallView.kt
@@ -19,3 +19,8 @@ val strokeFactor : Float = 90f
 val sizeFactor : Float = 5.8f
 val backColor : Int = Color.parseColor("#BDBDBD")
 val delay : Long = 20
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
