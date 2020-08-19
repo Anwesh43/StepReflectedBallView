037891fa2709b618ef8099d297d6f1a7476b39b1
Anwesh43 <anweshthecool0@gmail.com>
2020-08-20 04:47:35 +0530

    adding static create method

--- app/src/main/java/com/anwesh/uiprojects/linkedstepreflectedballview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedstepreflectedballview/MainActivity.kt
@@ -2,11 +2,19 @@ package com.anwesh.uiprojects.linkedstepreflectedballview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.stepreflectedballview.StepReflectedBallView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        StepReflectedBallView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
