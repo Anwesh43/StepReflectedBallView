4cb9481c88ccc56e28af1dcf3a06af31ffd28f59
Anwesh43 <anweshthecool0@gmail.com>
2020-08-20 04:36:28 +0530

    adding SRBNode class

--- stepreflectedballview/src/main/java/com/anwesh/uiprojects/stepreflectedballview/StepReflectedBallView.kt
+++ stepreflectedballview/src/main/java/com/anwesh/uiprojects/stepreflectedballview/StepReflectedBallView.kt
@@ -110,4 +110,45 @@ class StepReflectedBallView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SRBNode(var i : Int, val state : State = State()) {
+
+        private var next : SRBNode? = null
+        private var prev : SRBNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SRBNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSRBNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SRBNode {
+            var curr : SRBNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
