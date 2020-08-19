049ff80ee9c076ae8419be51a2d7be1826c77f3b
Anwesh43 <anweshthecool0@gmail.com>
2020-08-20 04:40:43 +0530

    adding StepReflecBall class

--- stepreflectedballview/src/main/java/com/anwesh/uiprojects/stepreflectedballview/StepReflectedBallView.kt
+++ stepreflectedballview/src/main/java/com/anwesh/uiprojects/stepreflectedballview/StepReflectedBallView.kt
@@ -151,4 +151,27 @@ class StepReflectedBallView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class StepReflectedBall(var i : Int) {
+
+        private var curr : SRBNode = SRBNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUdpating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
