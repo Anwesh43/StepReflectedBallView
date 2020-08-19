19734dfc517664eb7062ffbaf159bced4bbabe40
Anwesh43 <anweshthecool0@gmail.com>
2020-08-20 04:29:43 +0530

    adding StepReflectedBallView class

--- stepreflectedballview/src/main/java/com/anwesh/uiprojects/stepreflectedballview/StepReflectedBallView.kt
+++ stepreflectedballview/src/main/java/com/anwesh/uiprojects/stepreflectedballview/StepReflectedBallView.kt
@@ -47,3 +47,19 @@ fun Canvas.drawSRBNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawStepReflectedBall(scale, w, h, paint)
 }
+
+class StepReflectedBallView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
